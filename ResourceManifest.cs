50da3c557b42448800e0b15b52d5b09cb8206e92
matt4446 <barmy_matt@hotmail.com>
2012-05-02 12:30:30 +0100

    Orchard Kendo Ui (GPLV3) Web module.
    Signed-off-by: matt4446 <barmy_matt@hotmail.com>

---
+++ ResourceManifest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard.UI.Resources;
+
+namespace Kendo.Web
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            var mainScript = "kendo.web";
+
+            var version = "2012.1.327";
+
+            manifest.DefineScript(mainScript)
+                .SetUrl("kendo.all.min.js")
+                .SetDependencies("jQuery")
+                .SetVersion(version);
+
+            manifest.DefineStyle("kendo.web.common")
+                .SetUrl("kendo.common.min.css")
+                .SetVersion(version);
+
+            manifest.DefineStyle("kendo.web.default")
+                .SetUrl("kendo.default.min.css")
+                .SetVersion(version);
+
+            manifest.DefineStyle("kendo.web.black")
+                .SetUrl("kendo.black.min.css")
+                .SetVersion(version);
+
+            manifest.DefineStyle("kendo.web.blueopal")
+                .SetUrl("kendo.blueopal.min.css")
+                .SetVersion(version);
+
+            manifest.DefineStyle("kendo.web.metro")
+                .SetUrl("kendo.metro.min.css")
+                .SetVersion(version);
+
+            manifest.DefineStyle("kendo.web.silver")
+                .SetUrl("kendo.silver.min.css")
+                .SetVersion(version);
+        }
+
+    }
+}
