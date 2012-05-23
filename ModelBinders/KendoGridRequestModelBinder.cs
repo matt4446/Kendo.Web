97c99f4e4e1db92a46bc2501d57bd8eb3a3531c6
matt4446 <barmy_matt@hotmail.com>
2012-05-23 12:53:10 +0100

    Added a grid context binder - still testing with
    Signed-off-by: matt4446 <barmy_matt@hotmail.com>

---
+++ ModelBinders/KendoGridRequestModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KendoGridBinder;
+using Orchard.Mvc.ModelBinders;
+using Kendo.Web.Models;
+
+namespace Kendo.Web.ModelBinders
+{
+    public class KendoGridRequestModelBinder : IModelBinderProvider
+    {
+        public IEnumerable<ModelBinderDescriptor> GetModelBinders()
+        {
+            yield return new ModelBinderDescriptor() {
+                Type = typeof(KendoGridRequestOrchardModel),
+                ModelBinder = new KendoGridModelBinderOrchard()
+            };
+        }
+
+    }
+
+}
---
+++ Models/KendoGridRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KendoGridBinder;
+using System.Web.Mvc;
+using System.Web;
+
+namespace Kendo.Web.Models
+{
+    public class KendoGridRequestOrchardModel 
+    {
+        //public KendoGridRequest Model { get; set; }
+
+        //public KendoGridBinder.Containers.FilterObjectWrapper FilterObjectWrapper { get; set; }
+
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int Skip { get; set; }
+
+        //public IEnumerable<KendoGridBinder.Containers.SortObject> SortObjects { get; set; }
+
+        public int Take { get; set; }
+
+        public string Logic { get; set; }
+    }
+
+    public class KendoGridModelBinderOrchard : KendoGridModelBinder
+    {
+        private HttpRequestBase _request;
+
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            KendoGridRequest o = base.BindModel(controllerContext, bindingContext) as KendoGridRequest;
+            
+            return new KendoGridRequestOrchardModel() 
+            { 
+                 //FilterObjectWrapper = o.FilterObjectWrapper,
+                 Logic = o.Logic,
+                 Page = o.Page,
+                 PageSize = o.PageSize,
+                 Skip = o.Skip,
+                 //SortObjects = o.SortObjects,
+                 Take = o.Take
+            };
+        }
+    }
+}
